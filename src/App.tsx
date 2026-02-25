01086b719eb6776bad2e9cd4e017c5fb908fb9d9
gem1ineye <harshitnerd@gmail.com>
2026-02-25 09:19:37 +0530

    remove speed insight from app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <Analytics />
-    <SpeedInsights />
+    
     <TooltipProvider>
       <Toaster />
       <Sonner />
